d380afc2bfe770538cb09267779d32fa56cf2d94
Paul Talbot <paul.valentin.talbot@gmail.com>
2022-08-02 03:38:08 +0200

    initial front commit

---
+++ web/src/components/LoginForm.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="login-form">
+    <form>
+      <div class="login-form__item-container">
+        <label for="login-form__login">Login</label>
+        <input id="login-form__login" type="text">
+      </div>
+      <div class="login-form__item-container">
+        <label for="login-form__password">Password</label>
+        <input id="login-form__password" type="password">
+      </div>
+    </form>
+  </div>
+</template>
+
+<style scoped>
+.login-form {
+    margin: auto;
+}
+
+.login-form__item-container {
+    padding: 0 0 10px 0;
+    display: flex;
+    flex-flow: column nowrap;
+}
+
+form {
+    display: flex;
+    flex-flow: column nowrap;
+}
+
+label {
+    align-self: start;
+}
+</style>
---
+++ web/src/views/Login.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import LoginForm from "@/components/LoginForm.vue"
+</script>
+
+<template>
+  <div class="login-container">
+    <LoginForm />
+  </div>
+</template>
+
+<style scoped>
+.login-container {
+    height: 100%;
+    width: 100%;
+    display: flex;
+}
+</style>

3cccd4ef454f297bd1675a9d380f3fa02efee413
Paul Talbot <paul.valentin.talbot@gmail.com>
2022-08-06 05:18:07 +0200

    migration for expense category table

---
+++ internal/app/pkg/db/migrations/mysql/000006_create_expense_category_table.up.sql
@@ -0,0 +1,16 @@
+BEGIN;
+
+
+CREATE TABLE IF NOT EXISTS expenses_categories(
+  id INT NOT NULL UNIQUE AUTO_INCREMENT,
+  name VARCHAR (127),
+  PRIMARY KEY(id)
+);
+
+ALTER TABLE expenses
+ADD expense_category_id INT,
+ADD CONSTRAINT `fk_expense_expense_category`
+  FOREIGN KEY (expense_category_id) REFERENCES expenses_categories(id);
+
+
+COMMIT;

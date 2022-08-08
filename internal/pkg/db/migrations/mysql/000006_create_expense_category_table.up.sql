d5055cfae4d471ae3cff6a54d8fde505bfd18abb
Paul Talbot <paul.valentin.talbot@gmail.com>
2022-08-08 03:09:34 +0200

    add user to expense category

--- internal/pkg/db/migrations/mysql/000006_create_expense_category_table.up.sql
+++ internal/pkg/db/migrations/mysql/000006_create_expense_category_table.up.sql
@@ -4,7 +4,10 @@ BEGIN;
 CREATE TABLE IF NOT EXISTS expenses_categories(
   id INT NOT NULL UNIQUE AUTO_INCREMENT,
   name VARCHAR (127),
-  PRIMARY KEY(id)
+  user_id INT NOT NULL,
+  PRIMARY KEY(id),
+  CONSTRAINT `fk_expense_category_user`
+    FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
 ALTER TABLE expenses
